358b8caa7fff9a6bda3e51d383228ae57a8c5f5b
Moez <moez_zouari@hotmail.fr>
2021-06-29 12:35:21 +0100

    Sending Http Request to Our Api routes

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,23 +1,26 @@
 import { MongoClient } from "mongodb";
 
 // /api/new-meetup
+// POST /api/new-meetup
 
 async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
     const client = await MongoClient.connect(
-      "mongodb+srv://moez:moez@cluster0.7xdar.mongodb.net/meetups?retryWrites=true&w=majority"
+      "mongodb+srv://zmoez:zmoez@cluster0.7xdar.mongodb.net/meetups?retryWrites=true&w=majority"
     );
     const db = client.db();
+
     const meetupsCollection = db.collection("meetups");
+
     const result = await meetupsCollection.insertOne(data);
 
     console.log(result);
 
     client.close();
 
-    res.status(201).json({ message: "Meetup Inserted!" });
+    res.status(201).json({ message: "Meetup inserted!" });
   }
 }
 
